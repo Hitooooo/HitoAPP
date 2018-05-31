2757f5b46b65c69b2ea2c6551235909bde08219b
孟海涛 <haitao.meng@maobc.com>
2018-05-31 10:13:33 +0800

    修改依赖关系，新建lib

--- app/src/main/java/com/hito/hitoapp/MainActivity.java
+++ app/src/main/java/com/hito/hitoapp/MainActivity.java
@@ -1,7 +1,7 @@
 package com.hito.hitoapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * 主界面的支撑Activity
--- app/src/main/java/com/hito/hitoapp/SplashActivity.java
+++ app/src/main/java/com/hito/hitoapp/SplashActivity.java
@@ -1,14 +1,12 @@
-package com.hito.hitoapp.mvp.launch;
+package com.hito.hitoapp;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-
-import com.hito.hitoapp.R;
 
 /**
  * 广告也，可设置每次进入时是否有爱心小提示
  */
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
