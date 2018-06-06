34acb034131e291a79c41d6c32d3207e4be5c27b
mht <hitomeng@outlook.com>
2018-06-06 23:37:40 +0800

    提交网络框架

--- app/src/main/java/com/hito/hitoapp/SplashActivity.java
+++ app/src/main/java/com/hito/hitoapp/SplashActivity.java
@@ -1,12 +1,12 @@
 package com.hito.hitoapp;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * 广告也，可设置每次进入时是否有爱心小提示
  */
-public class SplashActivity extends Activity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ chatlib/src/main/java/com/hito/chatlib/net/RequestHandler.java
@@ -0,0 +1,11 @@
+package com.hito.chatlib.net;
+
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public interface RequestHandler {
+    Request onBeforeRequest(Request request, Interceptor.Chain chain);
+
+    Response onAfterRequest(Response response, Interceptor.Chain chain);
+}
